59567d4fe280acd6e35ec22f752b9910c848f933
jhonni-dev <jhonni.dev@gmail.com>
2021-12-29 11:44:42 -0300

    Implementação do menu e conceitos

---
+++ Conceito.cs
@@ -0,0 +1,13 @@
+namespace Revisao
+{
+    public enum Conceito
+    {
+        A,
+        B,
+        C,
+        #pragma warning disable format
+        D,
+        E
+        #pragma warning restore format
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,20 +4,81 @@
     {
         static void Main (string[] args)
         {
+            Aluno[] alunos = new Aluno[5];
+            var indiceAluno = 0;
             string opcaoUsuario = ObterOpcaoUsuario();
+            
 
             while (opcaoUsuario.ToUpper() != "X")
             {
                 switch (opcaoUsuario)
                 {
                     case "1":
-                        //TODO: Adicionar alunos
+                        Console.WriteLine("Informe o nome do aluno");
+                        var aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+                        
+
+                        Console.WriteLine("Informe a nota do Aluno");
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        } else
+                        {
+                            throw new ArgumentOutOfRangeException("Valor da nota deve ser decimal");
+                        }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+
                         break;
                     case "2":
-                        //TODO: listar alunos
+                        foreach (var a in alunos)
+                        {
+                            if (!string.IsNullOrEmpty(a.Nome))
+                            {
+                            Console.WriteLine($"ALUNO: {a.Nome} - NOTA: {a.Nota}");
+                            }
+                        }
                         break;
                     case "3":
-                        //TODO: Calcular média geral
+                        decimal notaTotal = 0;
+                        var nrAlunos = 0;
+                        for (int i=0; i<alunos.Length; i++)
+                        {
+                            if (!string.IsNullOrEmpty(alunos[i].Nome))
+                            {
+                                notaTotal = notaTotal + alunos[i].Nota;
+                                nrAlunos++;
+                            }
+                        }
+
+                        var mediaGeral = notaTotal / nrAlunos;
+                        Conceito conceitoGeral;
+
+                        if (mediaGeral<=2)
+                        {
+                            conceitoGeral = Conceito.E;
+                        }
+                        else if (mediaGeral<=4)
+                        {
+                            conceitoGeral = Conceito.D;
+                        }
+                        else if (mediaGeral<=6)
+                        {
+                            conceitoGeral = Conceito.C;
+                        }
+                        else if (mediaGeral<=8)
+                        {
+                            conceitoGeral = Conceito.B;
+                        }
+                        else
+                        {
+                            conceitoGeral = Conceito.A;
+                        }
+                        
+                        Console.WriteLine($"MÉDIA GERAL: {mediaGeral} - CONCEITO: {conceitoGeral}");
+                        
                         break;
                     default:
                         throw new ArgumentOutOfRangeException();
@@ -31,6 +92,7 @@
 
         private static string ObterOpcaoUsuario()
         {
+            Console.WriteLine();
             Console.WriteLine("Informe a opção desejada:");
             Console.WriteLine("1- Inserir novo aluno");
             Console.WriteLine("2- Listar alunos");
@@ -38,7 +100,8 @@
             Console.WriteLine("X- Sair");
             Console.WriteLine();
 
-            string opcaoUsuario = Console.ReadLine();
+            string? opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
             return opcaoUsuario;
         }
     }
